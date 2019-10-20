b23740dbb3c12e8f438d4ed472d61f7f327f0034
zhanggoodzhi <474294484@qq.com>
2019-10-20 22:51:27 +0800

    登录

--- src/view/signAccount/login/index.vue
+++ src/view/signAccount/login/index.vue
@@ -1,53 +1,100 @@
 <style lang="scss" scoped>
-  @import 'index';
+@import "index";
 </style>
 <template>
-    <div class="login">
-      <div class="login-wrapper">
-        <div class="login-logo">
-          <p class="login-logo-bg">logo</p>
-        </div>
-        <div class="login-main">
-          <div class="login-container">
-            <el-tabs v-model="opeList.tab.active" @tab-click="handleClick">
-              <el-tab-pane :label="opeList.tab.list[0].name" :name="opeList.tab.list[0].value">
-                <div>
-                  <el-input v-model="opeList.accountVerifyList.name.value" placeholder="请输入常用手机号/邮箱"></el-input>
-                </div>
-                <div>
-                  <el-input v-model="opeList.accountVerifyList.pwd.value" placeholder="请输入密码"></el-input>
-                </div>
-              </el-tab-pane>
-              <el-tab-pane :label="opeList.tab.list[1].name" :name="opeList.tab.list[1].value">fff</el-tab-pane>
-            </el-tabs>
+  <div class="login">
+    <div class="login-wrapper">
+      <img
+        class="wx-btn"
+        src="@/assets/register/code.png"
+      />
+      <img
+        class="wx-img"
+        src="@/assets/register/tuoyuan.png"
+        alt=""
+      >
+      <span class="wx-span">微信快速登录</span>
+      <img
+        class="man"
+        src="@/assets/register/man.png"
+        alt=""
+      >
+      <img
+        class="flower"
+        src="@/assets/register/flower.png"
+        alt=""
+      >
+      <img
+        class="women"
+        src="@/assets/register/women.png"
+        alt=""
+      >
+      <div class="center clearfix">
+        <div class="left">
+          <div class="title">
+            <span class="big">Beep 币扑</span>
+            <span class="split"></span>
+            <span class="small">区块链人专属社交网络</span>
           </div>
-          <div class="login-ways"></div>
+          <el-input
+            class="form-item"
+            placeholder="请输入常用手机号"
+            v-model="account"
+          >
+            <el-select
+              v-model="phonePre"
+              slot="prepend"
+              style="width:100px;"
+            >
+              <el-option
+                label="0086"
+                value="0086"
+              ></el-option>
+            </el-select>
+          </el-input>
+          <el-input
+            class="form-item"
+            placeholder="请输入验证码"
+            v-model="validateCode"
+          >
+            <el-button slot="append">获取验证码</el-button>
+          </el-input>
+          <div>
+            <el-button
+              class="register-btn color-btn"
+              type="primary"
+            >注册</el-button>
+          </div>
+          <div class="tip">注册代表你已同意 「Beep 币扑用户协议」</div>
+        </div>
+        <div class="right">
+          <div class="label">还没有账号:</div>
+          <el-button class="color-btn" type="primary">立即注册<i class="el-icon-right"></i></el-button>
+          <div class="label">
+            使用以下账号直接登录:
+          </div>
+          <div class="icon-wrap">
+            <img src="@/assets/register/wx.png" alt="">
+             <img src="@/assets/register/QQ.png" alt="">
+              <img src="@/assets/register/weibo.png" alt="">
+          </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      'opeList': {
-        'tab': {
-          'active': 'pwdWay',
-          'list': [ { 'name': '密码登录', 'value': 'pwdWay' }, { 'name': '验证码登录', 'value': 'codeWay' } ]
-        },
-        'accountVerifyList': {
-          'name': { 'value': '' },
-          'pwd': { 'value': '' }
-        },
-        'codeVerifyList': {
-          'pwd': ''
-        }
-      }
+      phonePre: '0086',
+      account: '',
+      validateCode: ''
     }
   },
   methods: {
-    handleClick (tab, event) {
+    handleClick(tab, event) {
       console.log(tab, event)
     }
   }
@@ -55,5 +102,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>

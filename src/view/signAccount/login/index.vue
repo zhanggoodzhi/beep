79bcc1a90d4bbc7031338bb8dd4dd14f01ae2a8f
zhanggoodzhi <474294484@qq.com>
2019-10-21 23:04:26 +0800

    登录页

--- src/view/signAccount/login/index.vue
+++ src/view/signAccount/login/index.vue
@@ -16,49 +16,59 @@
       <span class="wx-span">微信快速登录</span>
       <img
         class="man"
-        src="@/assets/register/man.png"
-        alt=""
-      >
-      <img
-        class="flower"
-        src="@/assets/register/flower.png"
+        src="@/assets/register/man2.png"
         alt=""
       >
       <img
         class="women"
-        src="@/assets/register/women.png"
+        src="@/assets/register/women2.png"
         alt=""
       >
       <div class="center clearfix">
         <div class="left">
-          <div class="title">
-            <span class="big">Beep 币扑</span>
-            <span class="split"></span>
-            <span class="small">区块链人专属社交网络</span>
-          </div>
-          <el-input
-            class="form-item"
-            placeholder="请输入常用手机号"
-            v-model="account"
-          >
-            <el-select
-              v-model="phonePre"
-              slot="prepend"
-              style="width:100px;"
-            >
-              <el-option
-                label="0086"
-                value="0086"
-              ></el-option>
-            </el-select>
-          </el-input>
-          <el-input
-            class="form-item"
-            placeholder="请输入验证码"
-            v-model="validateCode"
-          >
-            <el-button slot="append">获取验证码</el-button>
-          </el-input>
+          <el-tabs>
+            <el-tab-pane label="密码登录">
+              <el-input
+                class="form-item"
+                placeholder="请输入常用手机号/邮箱"
+                v-model="account"
+              >
+              </el-input>
+              <el-input
+                class="form-item"
+                type="password"
+                placeholder="请输入密码"
+                v-model="password"
+              >
+              </el-input>
+            </el-tab-pane>
+            <el-tab-pane label="验证码登录">
+              <el-input
+                class="form-item"
+                placeholder="请输入常用手机号"
+                v-model="phone"
+              >
+                <el-select
+                  v-model="phonePre"
+                  slot="prepend"
+                  style="width:100px;"
+                >
+                  <el-option
+                    label="0086"
+                    value="0086"
+                  ></el-option>
+                </el-select>
+              </el-input>
+              <el-input
+                class="form-item"
+                placeholder="请输入验证码"
+                v-model="validateCode"
+              >
+                <el-button slot="append">获取验证码</el-button>
+              </el-input>
+            </el-tab-pane>
+          </el-tabs>
+
           <div>
             <el-button
               class="register-btn color-btn"
@@ -69,14 +79,26 @@
         </div>
         <div class="right">
           <div class="label">还没有账号:</div>
-          <el-button class="color-btn" type="primary">立即注册<i class="el-icon-right"></i></el-button>
+          <el-button
+            class="color-btn"
+            type="primary"
+          >立即注册<i class="el-icon-right"></i></el-button>
           <div class="label">
             使用以下账号直接登录:
           </div>
           <div class="icon-wrap">
-            <img src="@/assets/register/wx.png" alt="">
-             <img src="@/assets/register/QQ.png" alt="">
-              <img src="@/assets/register/weibo.png" alt="">
+            <img
+              src="@/assets/register/wx.png"
+              alt=""
+            >
+            <img
+              src="@/assets/register/QQ.png"
+              alt=""
+            >
+            <img
+              src="@/assets/register/weibo.png"
+              alt=""
+            >
           </div>
         </div>
       </div>
@@ -88,8 +110,10 @@
 export default {
   data() {
     return {
-      phonePre: '0086',
       account: '',
+      password:'',
+      phonePre: '0086',
+      phone: '',
       validateCode: ''
     }
   },
